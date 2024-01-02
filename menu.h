710433628c3efd499bddd2ad9d412548e1e9f575
HADDANIAbdu <ab.haddani10@gmail.com>
2024-01-02 13:43:50 +0100

    Create text_art.h

---
+++ menu.h
@@ -0,0 +1,9 @@
+int menu_select(){
+    int choice = 0;
+    printf("\xB3 Let's Start Playing ...\n\n");
+    printf("1- Play Normal\n");
+    printf("2- Play with levels\n");
+    printf("3- Quit\n\n");
+    scanf("%d",&choice);
+    return choice;
+}
